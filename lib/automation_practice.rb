eea6d2b435a5fe1762ab4bdfeada24d8a72b3842
MashR3 <michaelash17@googlemail.com>
2017-10-25 17:53:50 +0100

    Begin

--- lib/automation_practice.rb
+++ lib/automation_practice.rb
@@ -1,4 +1,17 @@
-#  require relative's will go here for each page
+require_relative 'pages/homepage'
+
 
 class AutomationPracticeSite
+
+  def homepage
+    Homepage.new
+  end
+
+
+
+
+
+
+  
+
 end
---
+++ lib/pages/homepage.rb
@@ -0,0 +1,30 @@
+require 'capybara/dsl'
+
+class Homepage
+  include Capybara::DSL
+
+  # Page objects below
+  HOMEPAGE_URL = 'http://automationpractice.com/index.php'
+  SIGN_IN_LINK_ID = '#login'
+  CONTACT_US_LINK_ID = '#contact-link'
+
+  # Helper methods
+  def visit_home_page
+    visit(HOMEPAGE_URL)
+  end
+
+  def sign_in_link
+    find(SIGN_IN_LINK_ID)
+  end
+
+  def click_sign_in_link
+    sign_in_link.click
+  end
+
+  
+
+
+
+
+
+end
