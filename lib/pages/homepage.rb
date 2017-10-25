3aa1e64fd734fde73d6f65c16dac75a1db5a6691
MashR3 <michaelash17@googlemail.com>
2017-10-25 23:22:21 +0100

    Tests begun

--- lib/pages/homepage.rb
+++ lib/pages/homepage.rb
@@ -5,12 +5,20 @@ class Homepage
 
   # Page objects below
   HOMEPAGE_URL = 'http://automationpractice.com/index.php'
-  SIGN_IN_LINK_ID = '#login'
+  SIGN_IN_LINK_ID = '.login'
   CONTACT_US_LINK_ID = '#contact-link'
 
   # Helper methods
   def visit_home_page
     visit(HOMEPAGE_URL)
+  end
+
+  def contact_us_link
+    find(CONTACT_US_LINK_ID)
+  end
+  
+  def click_contact_us_link
+    contact_us_link.click
   end
 
   def sign_in_link
@@ -21,7 +29,7 @@ class Homepage
     sign_in_link.click
   end
 
-  
+
 
 
 
---
+++ spec/homepage_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe 'homepage' do
+
+  before(:all) do
+    @auto_site = AutomationPracticeSite.new
+  end
+
+  it 'should have a clickable "contact us" link' do
+    @auto_site.homepage.visit_home_page
+    @auto_site.homepage.click_contact_us_link
+  end
+
+  it 'should have a clickable "log in" link' do
+    @auto_site.homepage.visit_home_page
+    @auto_site.homepage.click_sign_in_link
+  end
+
+  it 'should have a sales promotion bar at the top of the screen' do
+    @auto_site.homepage.find(:xpath, '//*[@id="header"]/div[1]/div/div/a/img').visible?
+  end
+
+  it 'should have the site logo on the page' do
+    @auto_site.homepage.find(:xpath, '//*[@id="header_logo"]/a/img').visible?
+  end
+
+  # Top Search Bar Tests
+  it 'checks a search text field is present' do
+    @auto_site.homepage.find('#search_query_top').visible?
+  end
+
+  it 'checks the search submit button present' do
+    @auto_site.homepage.find(:xpath, '//*[@id="searchbox"]/button')
+  end
+
+
+
+end
