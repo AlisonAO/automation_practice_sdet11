0c84b5bd48aa9201432077414b11d87d54de81bd
AlisonAO <alison_opeloyeru@hotmail.co.uk>
2017-10-26 11:05:42 +0100

    added tests

--- spec/create_account_spec.rb
+++ spec/create_account_spec.rb
@@ -6,12 +6,21 @@ describe 'webpage' do
         @automation = AutomationPracticeSite.new
     end
 
+    it "should create account" do
+        @automation.login.visit_sign_in_page
+        sleep 5
+        @automation.login.input_new_user_email('code@hotmail.com')
+        @automation.login.click_create_submit_button
+        sleep 5
+    end
+
     it "should create an account" do
-        @automation.
-        @automation.
-        @automation.
-        @automation.
-        sleep 10
+        @automation.createaccount.choose_title_button('Mrs.')
+        @automation.createaccount.input_customer_firstname('Zara')
+        @automation.createaccount.input_customer_lastname('Swanson')
+        @automation.createaccount.input_email('code@hotmail.com')
+        @automation.createaccount.choose_day('option[2]')
+        sleep 5
     end
 
 
