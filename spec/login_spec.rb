491fef37cb4768ab18f56d1cf6ff6e3a3fd0eaaf
21294 <brian-higuita@hotmail.co.uk>
2017-10-26 11:05:12 +0100

    included create automation

--- spec/login_spec.rb
+++ spec/login_spec.rb
@@ -6,13 +6,19 @@ describe 'webpage' do
         @automation = AutomationPracticeSite.new
     end
 
-    it "should sign in" do
+    it "should create account" do
         @automation.login.visit_sign_in_page
-        @automation.login.input_user_email("test@spartaglobal.co")
-        @automation.login.input_user_password("password")
-        @automation.login.click_sign_in_submit_button
+        @automation.login.input_new_user_email('brian@sparta.com')
+        @automation.login.click_create_submit_button
         sleep 10
     end
+
+
+    # it "should sign in" do
+    #     @automation.login.input_user_email("test@spartaglobal.co")
+    #     @automation.login.input_user_password("password")
+    #     @automation.login.click_sign_in_submit_button
+    # end
 
 
 
