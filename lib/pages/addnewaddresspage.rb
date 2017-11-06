be0ee887785de398436119d023b52427444d8526
seb-faull <seb_faull@hotmail.co.uk>
2017-11-06 16:04:43 +0000

    finished making objects for make a new address page

--- lib/pages/addnewaddresspage.rb
+++ lib/pages/addnewaddresspage.rb
@@ -18,6 +18,7 @@ class AddNewAddressPage
   MOBILE_PHONE_FIELD_ID = '#phone_mobile'
   ADDITIONAL_INFORMATION_FIELD_ID = '#other'
   ASSIGN_AN_ADDRESS_ALIAS_FIELD_ID = '#alias'
+  SAVE_ADDRESS_LINK = '#submitAddress'
 
   #Helper methods below
     def visit_add_new_address_page
@@ -69,8 +70,71 @@ class AddNewAddressPage
     end
 
     def input_city_field(city)
-      find_city_field(city)
+      find_city_field.send_keys(city)
     end
 
+    def find_state_selector
+      find(STATE_SELECTOR_ID)
+    end
+
+    def select_state(state)
+      find_state_selector.select_by(state)
+    end
+
+    def find_postcode_field
+      find(POSTCODE_INPUT_FIELD_ID)
+    end
+
+    def input_postcode_field(postcode)
+      find_postcode_field.send_keys(postcode)
+    end
+
+    def find_country_selector
+      find(COUNTRY_SELECTOR_ID)
+    end
+
+    def select_country(country)
+      find_country_selector.select_by(country)
+    end
+
+    def find_home_phone_field
+      find(HOME_PHONE_FIELD_ID)
+    end
+
+    def input_home_phone_field(home_phone)
+      find_home_phone_field.send_keys(home_phone)
+    end
+
+    def find_mobile_phone_field
+      find(MOBILE_PHONE_FIELD_ID)
+    end
+
+    def input_mobile_phone_field(mobile_phone)
+      find_mobile_phone_field.send_keys(mobile_phone)
+    end
+
+    def find_additional_information_field
+      find(ADDITIONAL_INFORMATION_FIELD_ID)
+    end
+
+    def input_additional_information_field(additional_information)
+      find_additional_information_field.send_keys(additional_information)
+    end
+
+    def find_assign_an_address_alias_field
+      find(ASSIGN_AN_ADDRESS_ALIAS_FIELD_ID)
+    end
+
+    def input_assign_an_address_alias_field(alias_address)
+      find_assign_an_address_alias_field(alias_address)
+    end
+
+    def find_save_address_link
+      find(SAVE_ADDRESS_LINK)
+    end
+
+    def click_save_address_link
+      find_save_address_link.click
+    end
 
 end
