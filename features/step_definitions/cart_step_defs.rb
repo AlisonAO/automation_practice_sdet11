d88bb103b428109a736ae65e00b1405087cf7a3a
sbaxter95 <sbaxter95@outlook.com>
2017-11-06 19:43:02 +0000

    step definition for first scenario

--- features/step_definitions/cart_step_defs.rb
+++ features/step_definitions/cart_step_defs.rb
@@ -1,23 +1,25 @@
 Given("that I am on the t-shirt item page") do
-  pending # Write code here that turns the phrase above into concrete actions
+  @cart = Cart.new
+  @cart.load_item_page
+  expect(@cart.current_url).to eq('http://automationpractice.com/index.php?id_product=1&controller=product')
 end
 
 When("I click on the add to cart button") do
-  pending # Write code here that turns the phrase above into concrete actions
+  @cart.click_item_page
 end
 
 And("I click on the proceed to checkout button") do
-  pending # Write code here that turns the phrase above into concrete actions
+  @cart.click_proceed_to_checkout_button
 end
 
 Then("I will go to the checkout page") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(@cart.current_url).to eq('http://automationpractice.com/index.php?controller=order')
 end
 
-When("I click on the return to shopping button") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then("I will go back to the t-shirt item page") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
+# When("I click on the return to shopping button") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# Then("I will go back to the t-shirt item page") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
