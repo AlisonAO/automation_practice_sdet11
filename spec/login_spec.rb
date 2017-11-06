d5ca7a44b44f4226b106280b583b824430ba3b5a
21294 <brian-higuita@hotmail.co.uk>
2017-11-06 14:44:27 +0000

    changing branch

--- spec/login_spec.rb
+++ spec/login_spec.rb
@@ -10,16 +10,15 @@ describe 'webpage' do
         @automation.login.visit_sign_in_page
         @automation.login.input_new_user_email('brian@sparta.com')
         @automation.login.click_create_submit_button
-        sleep 10
+        sleep 3
     end
 
 
-    # it "should sign in" do
-    #     @automation.login.input_user_email("test@spartaglobal.co")
-    #     @automation.login.input_user_password("password")
-    #     @automation.login.click_sign_in_submit_button
-    # end
-
-
+    it "should sign in" do
+        @automation.login.visit_sign_in_page
+        @automation.login.input_user_email("test@spartaglobal.co")
+        @automation.login.input_user_password("password")
+        @automation.login.click_sign_in_submit_button
+    end
 
 end
