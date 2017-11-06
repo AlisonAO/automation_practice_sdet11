bbfb85e3379747bfb48547e621d1fd3cf372b374
sbaxter95 <sbaxter95@outlook.com>
2017-11-06 16:58:00 +0000

    added step definition file

---
+++ features/step_definitions/cart_step_defs.rb
@@ -0,0 +1,25 @@
+You can implement step definitions for undefined steps with these snippets:
+
+Given("that I am on the t-shirt item page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click on the add to cart button") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click on the proceed to checkout button") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I will go to the checkout page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click on the return to shopping button") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I will go back to the t-shirt item page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
