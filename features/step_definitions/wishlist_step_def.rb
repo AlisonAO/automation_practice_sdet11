47ac3c1151972ae04747f9153411645659f42a59
Christian Dacres <christian_dacres@hotmail.co.uk>
2017-11-08 16:49:49 +0000

    add product to wishlist tests added

--- features/step_definitions/wishlist_step_def.rb
+++ features/step_definitions/wishlist_step_def.rb
@@ -49,3 +49,42 @@ end
 Then("that wishlist will be deleted") do
   expect(@site.mywishlistpage.find_delete_wishlist_icon).to eq nil
 end
+
+Given("I am logged in and on a product page") do
+  @site = AutomationPracticeSite.new
+  @site.login.visit_sign_in_page
+  @site.login.input_user_email("test@spartaglobal.co")
+  @site.login.input_user_password("password")
+  @site.login.click_sign_in_submit_button
+  @site.productpage.visit_product_page
+end
+
+When("I click the add to wishlist button") do
+  @site.productpage.click_add_to_wishlist_button
+end
+
+Then("I will get an added to wishlist message") do
+  @site.productpage.added_to_list_alert
+  @site.productpage.added_to_list_alert.click
+end
+
+Given("I have added a product to my wishlist") do
+  @site = AutomationPracticeSite.new
+  @site.login.visit_sign_in_page
+  @site.login.input_user_email("test@spartaglobal.co")
+  @site.login.input_user_password("password")
+  @site.login.click_sign_in_submit_button
+  @site.productpage.visit_product_page
+  @site.productpage.click_add_to_wishlist_button
+  @site.productpage.added_to_list_alert.click
+end
+
+When("I go to my my wishlist page and click on my wishlist") do
+  @listname = 'My Wishlist'
+  @site.mywishlistpage.visit_wishlist_page
+  @site.mywishlistpage.click_my_wishlist(@listname)
+end
+
+Then("I will see the product in my wishlist") do
+  @site.mywishlistpage.find_wishlist_item
+end
--- features/wishlist.feature
+++ features/wishlist.feature
@@ -2,15 +2,25 @@ Feature: Create and Delete Wishlist
 as a user
 i want to be able to create and delete wishlists
 
-  Scenario:  If I'm logged in I can access the my wishlist page.
+  Scenario: If I'm logged in I can access the my wishlist page.
     Given I am logged in and on the my account page
     When I click the my wishlists button
     Then I will be redirected to the my wishlist page
 
-  Scenario:  If I'm on the my wishlist page I can create a new wishlist.
+  Scenario: If I'm on the my wishlist page I can create a new wishlist.
     Given I am logged in and on the my wishlist page
     When I enter a wishlist name and click save
     Then it will create a new wishlist with that name
+
+  Scenario: If I'm on a product page I can add a product to my wishlist
+    Given I am logged in and on a product page
+    When I click the add to wishlist button
+    Then I will get an added to wishlist message
+
+  Scenario: If I've added a product to my wishlist then I should see it in the wishlist
+    Given I have added a product to my wishlist
+    When I go to my my wishlist page and click on my wishlist
+    Then I will see the product in my wishlist
 
   Scenario: If I'm on the my wishlist page I can delete a wishlist if there is one.
     Given I am logged in and on the my wishlist page
