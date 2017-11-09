93cfc8b6b8e218e8c4f06147685052a16b0081d5
ahawkins94 <adamhawkins94@hotmail.co.uk>
2017-11-09 16:39:38 +0000

    filled in the step defs and remade the features

--- features/step_definitions/checkout_step_defs.rb
+++ features/step_definitions/checkout_step_defs.rb
@@ -1,65 +1,46 @@
-Given("Items are added to the basket") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
 
 Given("I am on the checkout summary page") do
-  pending # Write code here that turns the phrase above into concrete actions
+  @checkout = AutomationPracticeSite.new
+  @checkout.checkout.visit_checkout
+  sleep 3
 end
 
 When("I click the proceed button") do
-  pending # Write code here that turns the phrase above into concrete actions
+  @checkout.checkout.checkout_button.click
+  sleep 3
 end
 
 Then("I go to the address confirmation page") do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-Given("I am on the address confirmation page") do
-  pending # Write code here that turns the phrase above into concrete actions
+Given("I enter a message") do
+  @checkout = AutomationPracticeSite.new
+  @checkout.checkout.input_checkout_field('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
+  sleep 3
 end
 
-When("I add a comment and click proceed to checkout") do
-  pending # Write code here that turns the phrase above into concrete actions
+When("I click proceed") do
+  @checkout.checkout.checkout_button_two.click
+  sleep 3
 end
 
-Then("I go to the shipping page") do
-  pending # Write code here that turns the phrase above into concrete actions
+Then("I will go to the shipping page") do
+  @checkout.checkout.checkout_button_three.click
 end
 
-Given("I am on the shipping page") do
-
-  pending # Write code here that turns the phrase above into concrete actions
+Given("I proceed through the pages") do
+  @checkout = AutomationPracticeSite.new
 end
 
 When("I check the terms of service checkbox") do
-  pending # Write code here that turns the phrase above into concrete actions
+  @checkout.checkout.tos_checkbox.click
+  sleep 3
 end
 
-Then("I can proceed to checkout") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Given("I am on the payments page") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-When("I click pay by wire") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then("I go to the order confirmation page") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Given("I am on the order confirmation page") do
-
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-When("I click confirm order") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then("I see a summary of my order") do
-  pending # Write code here that turns the phrase above into concrete actions
+Then("I will be able to confirm my order") do
+  @checkout.checkout.payment_method_1.click
+  sleep 3
+  @checkout.checkout.confirm_order.click
+  sleep 3
 end
