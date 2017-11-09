bfb9e1162461c3afbbc9252b651a709483fc2f14
sbaxter95 <sbaxter95@outlook.com>
2017-11-09 20:37:16 +0000

    made changes

--- features/step_definitions/cart_step_defs.rb
+++ features/step_definitions/cart_step_defs.rb
@@ -1,14 +1,16 @@
 Given("that I am on the t-shirt item page") do
-  @cart = Cart.new
+  @cart = AutomationPracticeSite.new.cart
   @cart.load_item_page
   expect(@cart.current_url).to eq('http://automationpractice.com/index.php?id_product=1&controller=product')
 end
 
 When("I click on the add to cart button") do
+  expect(@cart.add_to_cart_button_item_page).not_to be nil
   @cart.click_item_page
 end
 
 And("I click on the proceed to checkout button") do
+  expect(@cart.proceed_to_checkout_button).not_to be nil
   @cart.click_proceed_to_checkout_button
 end
 
@@ -17,6 +19,7 @@ Then("I will go to the checkout page") do
 end
 
 When("I click on the return to shopping button") do
+  expect(@cart.continue_shopping_button).not_to be nil
   @cart.click_continue_shopping_button
 end
 
